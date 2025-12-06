c1b052233b69e0f006084e70fb57aa15399aa941
Nils-Emil Lille <nilsemil.lille@gmail.com>
2025-12-06 14:25:29 +0200

    Change aftercare content

--- app/[locale]/(image-background)/page.tsx
+++ app/[locale]/(image-background)/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="relative min-h-screen flex flex-col">
       <div className="flex-1 flex items-center justify-center">
         <div className="grid grid-cols-1 lg:grid-cols-3 items-center gap-8 w-full px-12 text-center lg:text-left">
-          <div className="text-2xl md:text-4xl tracking-[.4em] color-tarvo lg:text-right landing-fade-in">
+          <div className="text-2xl md:text-4xl tracking-[.4em] color-tarvo lg:text-right">
             BASEL
           </div>
           <div className="relative w-32 h-32 md:w-48 md:h-48 mx-auto">
@@ -12,10 +12,10 @@ export default function Home() {
               alt="Tattoo Artist Logo"
               width={200}
               height={200}
-              className="w-full h-full landing-fade-in"
+              className="w-full h-full"
             />
           </div>
-          <div className="text-2xl md:text-4xl tracking-[.4em] color-tarvo lg:text-left landing-fade-in">
+          <div className="text-2xl md:text-4xl tracking-[.4em] color-tarvo lg:text-left">
             TALLINN
           </div>
         </div>
